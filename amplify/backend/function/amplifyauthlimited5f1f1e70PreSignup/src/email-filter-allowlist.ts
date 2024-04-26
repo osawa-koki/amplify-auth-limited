5e90cbff5c537911e90394496a9c6f84c6b525df
osawa-koki <koki@osawa.dev>
2024-04-26 18:41:01 +0900

    リファクタリング。

--- amplify/backend/function/amplifyauthlimited5f1f1e70PreSignup/src/email-filter-allowlist.ts
+++ amplify/backend/function/amplifyauthlimited5f1f1e70PreSignup/src/email-filter-allowlist.ts
@@ -1,22 +1,18 @@
-// @ts-nocheck
+import dotenv from 'dotenv'
+import { PreSignUpTriggerEvent } from 'aws-lambda'
 
-import dotenv from 'dotenv';
+dotenv.config()
 
-dotenv.config();
+const ALLOWED_EMAIL_REGEX_LIST = (process.env.ALLOWED_EMAIL_REGEX_LIST ?? '').split(',').map((d) => d.trim())
 
-const ALLOWED_EMAIL_REGEX_LIST = process.env.ALLOWED_EMAIL_REGEX_LIST.split(',').map((d) => d.trim());
+exports.handler = async (event: PreSignUpTriggerEvent) => {
+  const { email } = event.request.userAttributes
 
-/**
- * @type {import('@types/aws-lambda').PreSignUpTriggerHandler}
- */
-exports.handler = async (event) => {
-  const { email } = event.request.userAttributes;
-
-  const isAllowed = ALLOWED_EMAIL_REGEX_LIST.some((regex) => new RegExp(regex).test(email));
+  const isAllowed = ALLOWED_EMAIL_REGEX_LIST.some((regex) => new RegExp(regex).test(email))
 
   if (!isAllowed) {
-    throw new Error(`\nallowed email regex list: ${ALLOWED_EMAIL_REGEX_LIST.join(', ')}`);
+    throw new Error(`\nallowed email regex list: ${ALLOWED_EMAIL_REGEX_LIST.join(', ')}`)
   }
 
-  return event;
-};
+  return event
+}
