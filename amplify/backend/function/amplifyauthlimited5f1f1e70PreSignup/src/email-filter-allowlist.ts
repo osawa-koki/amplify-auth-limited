1575ba74b8b80f62a6b37e48cbea5a4ae98ac7d8
osawa-koki <koki@osawa.dev>
2024-04-26 18:24:41 +0900

    環境変数をdotenvから取得するように。

--- amplify/backend/function/amplifyauthlimited5f1f1e70PreSignup/src/email-filter-allowlist.ts
+++ amplify/backend/function/amplifyauthlimited5f1f1e70PreSignup/src/email-filter-allowlist.ts
@@ -1,5 +1,11 @@
 // @ts-nocheck
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const ALLOWED_EMAIL_REGEX_LIST = process.env.ALLOWED_EMAIL_REGEX_LIST.split(',').map((d) => d.trim());
+
 /**
  * @type {import('@types/aws-lambda').PreSignUpTriggerHandler}
  */
@@ -11,7 +17,7 @@ exports.handler = async (event) => {
   const isAllowed = allowedEmailRegexList.some((regex) => new RegExp(regex).test(email));
 
   if (!isAllowed) {
-    throw new Error('Email address is not allowed');
+    throw new Error(ALLOWED_EMAIL_REGEX_LIST.join(', ') + ' are the only allowed domains');
   }
 
   return event;
