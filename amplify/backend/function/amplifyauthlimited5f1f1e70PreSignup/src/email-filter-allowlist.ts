7f1eef0a0979531f29275a9537a5c5c7529bc6b0
osawa-koki <koki@osawa.dev>
2024-04-26 19:15:42 +0900

    シークレットパスを環境変数に指定。

--- amplify/backend/function/amplifyauthlimited5f1f1e70PreSignup/src/email-filter-allowlist.ts
+++ amplify/backend/function/amplifyauthlimited5f1f1e70PreSignup/src/email-filter-allowlist.ts
@@ -1,7 +1,20 @@
 import dotenv from 'dotenv'
 import { PreSignUpTriggerEvent } from 'aws-lambda'
+import AWS from 'aws-sdk'
+
+const ssm = new AWS.SSM()
 
 dotenv.config()
+
+const getSecret = async (secretName: string): Promise<string | null> => {
+  const secretPath = process.env[secretName]
+  if (secretPath == null) return null
+  const { Parameter } = await ssm.getParameter({
+    Name: secretPath,
+    WithDecryption: true
+  }).promise()
+  return Parameter?.Value ?? null
+}
 
 const ALLOWED_EMAIL_REGEX_LIST = (process.env.ALLOWED_EMAIL_REGEX_LIST ?? '').split(',').map((d) => d.trim())
 
@@ -9,6 +22,8 @@ exports.handler = async (event: PreSignUpTriggerEvent) => {
   const { email } = event.request.userAttributes
 
   const isAllowed = ALLOWED_EMAIL_REGEX_LIST.some((regex) => new RegExp(regex).test(email))
+
+  throw new Error(JSON.stringify(process.env))
 
   if (!isAllowed) {
     throw new Error(`\nallowed email regex list: ${ALLOWED_EMAIL_REGEX_LIST.join(', ')}`)
